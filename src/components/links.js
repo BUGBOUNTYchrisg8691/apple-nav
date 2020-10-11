1fd6a51820643043ec2e5a0b0ff9758463ca7c31
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-10-11 15:29:28 -0400

    cropped ipad icons

--- src/components/links.js
+++ src/components/links.js
@@ -6,256 +6,32 @@ export const links = [{
     {
         text: "Mac",
         icon: "",
-        path: "mac/",
-        sublinks: [{
-            text: "MacBook Air",
-            icon: ""
-        },
-        {
-            text: 'MacBook Pro 13”',
-            icon: ""
-        },
-        {
-            text: 'MacBook Pro 16”',
-            icon: ""
-        },
-        {
-            text: "iMac New",
-            icon: ""
-        },
-        {
-            text: "iMac Pro",
-            icon: ""
-        },
-        {
-            text: "Mac Pro",
-            icon: ""
-        },
-        {
-            text: "Mac Mini",
-            icon: ""
-        },
-        {
-            text: "Compare",
-            icon: ""
-        },
-        {
-            text: "Pro Display XDR",
-            icon: ""
-        },
-        {
-            text: "Accessories",
-            icon: ""
-        },
-        {
-            text: "Catalina",
-            icon: ""
-        }]
+        path: "mac/"
     },
     {
         text: "iPad",
         icon: "",
-        path: "ipad/",
-        sublinks: [{
-            text: "iPad Pro",
-            icon: ""
-        },
-        {
-            text: "iPad Air New",
-            icon: ""
-        },
-        {
-            text: "iPad New",
-            icon: ""
-        },
-        {
-            text: "iPad mini",
-            icon: ""
-        },
-        {
-            text: "Compare",
-            icon: ""
-        },
-        {
-            text: "Apple Pencil",
-            icon: ""
-        },
-        {
-            text: "Keyboards",
-            icon: ""
-        },
-        {
-            text: "AirPods",
-            icon: ""
-        },
-        {
-            text: "Accessories",
-            icon: ""
-        },
-        {
-            text: "iPadOS",
-            icon: ""
-        }]
+        path: "ipad/"
     },
     {
         text: "iPhone",
         icon: "",
-        path: "iphone/",
-        sublinks: [{
-            text: "iPhone 11 Pro",
-            icon: ""
-        },
-        {
-            text: "iPhone 11",
-            icon: ""
-        },
-        {
-            text: "iPhone SE New",
-            icon: ""
-        },
-        {
-            text: "iPhone XR",
-            icon: ""
-        },
-        {
-            text: "Compare",
-            icon: ""
-        },
-        {
-            text: "AirPods",
-            icon: ""
-        },
-        {
-            text: "Accessories",
-            icon: ""
-        },
-        {
-            text: "Apple Card",
-            icon: ""
-        },
-        {
-            text: "iOS 14",
-            icon: ""
-        }]
+        path: "iphone/"
     },
     {
         text: "Watch",
         icon: "",
-        path: "watch/",
-        sublinks: [{
-            text: "Apple Watch Series 6 New",
-            icon: ""
-        },
-        {
-            text: "Apple Watch SE New",
-            icon: ""
-        },
-        {
-            text: "Apple Watch Series 3",
-            icon: ""
-        },
-        {
-            text: "Apple Watch Nike New",
-            icon: ""
-        },
-        {
-            text: "Apple Watch Hermès New",
-            icon: ""
-        },
-        {
-            text: "Apple Watch Studio",
-            icon: ""
-        },
-        {
-            text: "Compare",
-            icon: ""
-        },
-        {
-            text: "Bands",
-            icon: ""
-        },
-        {
-            text: "AirPods",
-            icon: ""
-        },
-        {
-            text: "Accessories",
-            icon: ""
-        },
-        {
-            text: "Apple Fitness+ Coming Soon",
-            icon: ""
-        },
-        {
-            text: "watchOS",
-            icon: ""
-        }]
+        path: "watch/"
     },
     {
         text: "TV",
         icon: "",
-        path: "tv/",
-        sublinks: [{
-            text: "Apple TV+",
-            icon: ""
-        },
-        {
-            text: "Apple TV App",
-            icon: ""
-        },
-        {
-            text: "Apple TV 4K",
-            icon: ""
-        },
-        {
-            text: "Apple TV HD",
-            icon: ""
-        },
-        {
-            text: "AirPlay",
-            icon: ""
-        },
-        {
-            text: "Accessories",
-            icon: ""
-        }]
+        path: "tv/"
     },
     {
         text: "Music",
         icon: "",
-        path: "music/",
-        sublinks: [{
-            text: "Apple Music",
-            icon: ""
-        },
-        {
-            text: "AirPods Pro",
-            icon: ""
-        },
-        {
-            text: "AirPods",
-            icon: ""
-        },
-        {
-            text: "HomePod",
-            icon: ""
-        },
-        {
-            text: "iPod Touch",
-            icon: ""
-        },
-        {
-            text: "Beats",
-            icon: ""
-        },
-        {
-            text: "Music Accessories",
-            icon: ""
-        },
-        {
-            text: "Gift Cards",
-            icon: ""
-        }]
+        path: "music/"
     },
     {
         text: "Support",
